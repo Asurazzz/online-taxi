b06cfb2d8e1dcb57683b92d33949451c3fdf160d
ymj <2513667783@qq.com>
2021-08-10 20:17:40 +0800

    校验controller

---
+++ service-verification-code/src/main/java/com/ymj/serviceverificationcode/controller/VerifyCodeController.java
@@ -0,0 +1,15 @@
+package com.ymj.serviceverificationcode.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname VerifyCodeController
+ * @Description TODO
+ * @Date 2021/8/10 19:44
+ * @Created by yemingjie
+ */
+@RestController
+@RequestMapping("/verify-code")
+public class VerifyCodeController {
+}
